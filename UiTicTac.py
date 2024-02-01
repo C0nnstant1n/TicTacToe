af314a173692b4cbd8d87669fd81a5923a0e0246
C0nstant1n <c0nnstant1n.cs@gmail.com>
2024-02-01 19:16:11 +0700

    пишу сигналы и обработчики для поля ввода

--- UiTicTac.py
+++ UiTicTac.py
@@ -1,19 +1,7 @@
 from PyQt6 import QtWidgets, QtGui, QtCore
 import sys
 from loadStyles import parse_css_styles
-
-
-class MainWindow(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
-        self.setWindowTitle('Крестики - нолики')
-        self.setFixedSize(820, 480)
-        pal = self.palette()
-        pal.setBrush(QtGui.QPalette.ColorGroup.Normal, QtGui.QPalette.ColorRole.Window,
-                     QtGui.QBrush(QtGui.QPixmap('img/math_list.png')))
-        pal.setBrush(QtGui.QPalette.ColorGroup.Inactive, QtGui.QPalette.ColorRole.Window,
-                     QtGui.QBrush(QtGui.QPixmap('img/math_list.png')))
-        self.setPalette(pal)
+from ticTacToe import game_desk
 
 
 class GridWidget(QtWidgets.QLabel):
@@ -36,12 +24,6 @@ class GridWidget(QtWidgets.QLabel):
         painter.end()
 
 
-class UiDesk(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
-        self.setGeometry(142, 184, 147, 146)
-
-
 class Field(QtWidgets.QLabel):
     click_signal = QtCore.pyqtSignal(int)
 
@@ -51,11 +33,61 @@ class Field(QtWidgets.QLabel):
         if 9 < idx < 0:
             idx = 0
         self.id = idx
+        self.active = False
 
     def mousePressEvent(self, evt):
         self.click_signal.emit(self.id)
         QtWidgets.QLabel.mousePressEvent(self, evt)
-        print('click', self.id)
+
+    def is_active(self):
+        return self.active
+
+    def set_active(self):
+        self.active = True
+
+
+class UiDesk(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.setGeometry(142, 184, 147, 146)
+        grid = QtWidgets.QGridLayout()
+        grid.setSpacing(3)
+        grid.setContentsMargins(0, 0, 0, 0)
+        self.fields = [Field(i) for i in range(0, 10)]
+        x = 0
+        for i in range(0, 3):
+            for j in range(0, 3):
+                grid.addWidget(self.fields[x], i, j)
+                x += 1
+        for field in self.fields:
+            field.setPixmap(QtGui.QPixmap('img/field.png'))
+            field.click_signal.connect(self.change_field)
+        self.setLayout(grid)
+
+    def change_field(self, id):
+        if not(self.fields[id].is_active()):
+            self.fields[id].set_active()
+            self.fields[id].setPixmap((QtGui.QPixmap('')))
+            print(id)
+        else:
+            return self.fields[id].is_active()
+
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.setWindowTitle('Крестики - нолики')
+        self.setFixedSize(820, 480)
+        pal = self.palette()
+        pal.setBrush(QtGui.QPalette.ColorGroup.Normal, QtGui.QPalette.ColorRole.Window,
+                     QtGui.QBrush(QtGui.QPixmap('img/math_list.png')))
+        pal.setBrush(QtGui.QPalette.ColorGroup.Inactive, QtGui.QPalette.ColorRole.Window,
+                     QtGui.QBrush(QtGui.QPixmap('img/math_list.png')))
+        self.setPalette(pal)
+        self.header = QtWidgets.QLabel('Крестики - нолики', self)
+        self.header.setStyleSheet(parse_css_styles('h1', 'styles/styles.css'))
+        self.header.setGeometry(260, 50, 300, 30)
+        self.grid_widget = GridWidget(self)
 
 
 if __name__ == '__main__':
@@ -63,29 +95,7 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
     window = MainWindow()
-
-    label = QtWidgets.QLabel('Крестики - нолики', window)
-    label.setStyleSheet(parse_css_styles('h1', 'styles/styles.css'))
-    label.setGeometry(260, 50, 300, 30)
-
-    grid = GridWidget(window)
-    desk = UiDesk(window)
-    desk_grid = QtWidgets.QGridLayout()
-    desk_grid.setContentsMargins(0, 0, 0, 0)
-    desk_grid.setSpacing(3)
-
-    x = 0
-    for i in range(0, 3):
-        for j in range(0, 3):
-
-            field = Field(x)
-            field.setObjectName(str(x))
-            field.setPixmap(QtGui.QPixmap('img/field.png'))
-            x += 1
-            # field.setStyleSheet('background-color: gray')
-
-            desk_grid.addWidget(field, i, j)
-    desk.setLayout(desk_grid)
+    ui = UiDesk(window)
 
     window.show()
     sys.exit(app.exec())
